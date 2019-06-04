f800370cef5e7eb884cff9fed3963ec57f0c4c52
theo <theoxao@163.com>
2019-06-04 19:55:52 +0800

    update

--- src/main/kotlin/com/theoxao/antlr/asyncript/ACParser.kt
+++ src/main/kotlin/com/theoxao/antlr/asyncript/ACParser.kt
@@ -1,6 +1,5 @@
 package com.theoxao.antlr.asyncript
 
-import com.theoxao.antlr.coffe.BaseParseTreeListener
 import com.theoxao.antlr.coffe.CoffeeBeanListener
 import com.theoxao.antlr.source.AsyncriptLexer
 import com.theoxao.antlr.source.AsyncriptParser
@@ -21,7 +20,7 @@ class ACParser {
     @Test
     fun test() {
         @Language("Groovy")
-        val script = "package com.example;\nimport com.theoxao.common.CommonResult;\nimport com.theoxao.common.ParamWrap;\n\n@RequestMapping\npublic  class Foo{\n  public  static void service(@Validated ParamWrap paramWrap, String s) async {\n        @Field final List<String> result = new ArrayList<String>();\n        String localUser = expect paramWrap.servicesHolder.httpClient.getFuture(\"http://git.theoxao.com\");\n        result.add(localUser);\n        String response = expect paramWrap.servicesHolder.httpClient.getFuture(\"http://git.theoxao.com\");\n        def user = ObjectMapper().readValue(response, User.class); result.add(user); \n        return new CommonResult(result);\n    }\n}\n\nstatic CommonResult service2( ParamWrap paramWrap ,@Validated String str)  {\n    List<String> result = new ArrayList<String>();\n    String localUser = expect paramWrap.servicesHolder.httpClient.getFuture(\"http://git.theoxao.com\");\n    result.add(localUser);\n    String response = expect paramWrap.servicesHolder.httpClient.getFuture(\"http://git.theoxao.com\");\n    def user = ObjectMapper().readValue(response, User.class); result.add(user); \n    return new CommonResult(result);\n}\n"
+        val script = "package com.example;\nimport com.theoxao.common.CommonResult;\nimport com.theoxao.common.ParamWrap;\n\n@RequestMapping\npublic  class Foo{\n  public  static List<String> service(@Validated ParamWrap paramWrap, String s) async {\n        @Field final List<String> result = new ArrayList<String>();\n        String url = \"http://git.theoxao.com\"\n        String localUser = expect paramWrap.servicesHolder.httpClient.getFuture(url);\n        result.add(localUser);\n        String response = expect paramWrap.servicesHolder.httpClient.getFuture(url);\n        def user = ObjectMapper().readValue(response, User.class); result.add(user); \n        return new CommonResult(result);\n    }\n}\n\nstatic CommonResult service2( ParamWrap paramWrap ,@Validated String str)  {\n    List<String> result = new ArrayList<String>();\n    String localUser = expect paramWrap.servicesHolder.httpClient.getFuture(\"http://git.theoxao.com\");\n    result.add(localUser);\n    String response = expect paramWrap.servicesHolder.httpClient.getFuture(\"http://git.theoxao.com\");\n    def user = ObjectMapper().readValue(response, User.class); result.add(user); \n    return new CommonResult(result);\n}\n"
         val parser = parse(script)
         val compilationUnit = parser.compilationUnit()
         ParseTreeWalker.DEFAULT.walk(CoffeeBeanListener(), compilationUnit)
@@ -34,8 +33,9 @@ class ACParser {
         val tokenStream: TokenStream = CommonTokenStream(lexer)
         val parser = AsyncriptParser(tokenStream)
         parser.removeErrorListeners()
-        parser.addErrorListener(DiagnosticErrorListener())
-        parser.errorHandler = DefaultErrorStrategy()
+//        parser.addErrorListener(DiagnosticErrorListener())
+//        parser.errorHandler = DefaultErrorStrategy()
+        parser.errorHandler = BailErrorStrategy()
         parser.addErrorListener(ConsoleErrorListener())
         return parser
     }
--- src/main/kotlin/com/theoxao/antlr/coffe/CoffeeBeanListener.kt
+++ src/main/kotlin/com/theoxao/antlr/coffe/CoffeeBeanListener.kt
@@ -12,6 +12,11 @@ import org.stringtemplate.v4.STGroupFile
 class CoffeeBeanListener : AsyncriptBaseListener() {
 
     private var currentAsyncMethodBody: AsyncriptParser.AsyncMethodBodyContext? = null
+    private var currentFieldNameElement: Element? = null
+    private var fieldList: MutableMap<String, Pair<Element?, Element?>?> = mutableMapOf()
+    private var asyncFieldList: MutableMap<String, Pair<Element?, Element?>?> = mutableMapOf()
+    private var currentField: Pair<Element?, Element?>? = null
+    private var currentTypeDeclarationElement: Element? = null
     private val code = StringBuilder()
     private val stg = STGroupFile("D:\\workspace\\prod\\script-as-service\\src\\main\\kotlin\\com\\theoxao\\antlr\\coffe\\coffe_bean.stg")
 
@@ -69,14 +74,22 @@ class CoffeeBeanListener : AsyncriptBaseListener() {
         if (modifiers != null && modifiers.isNotEmpty()) {
             code.append("${modifiers.reduce { acc, s -> "$acc $s" }} ")
         }
-        code.append("${ctx?.typeType()?.text} ")
-
+        ctx?.typeType()?.let {
+            val start = code.length
+            code.append("${it.text} ")
+            currentTypeDeclarationElement = Element(it.text, start, code.length - 1)
+        }
     }
 
     override fun enterVariableDeclaratorId(ctx: AsyncriptParser.VariableDeclaratorIdContext?) {
         if (ctx?.parent?.parent?.parent !is AsyncriptParser.FormalParametersContext)  //ignore since its already append in there
             ctx?.Identifier()?.let {
-                code.append("${it.text} = ")
+                val start = code.length
+                code.append(it.text)
+                currentFieldNameElement = Element(it.text, start, code.length)
+                code.append(" = ")
+                currentField = Pair(currentTypeDeclarationElement, currentFieldNameElement)
+                fieldList[it.text] = (currentField)
             }
     }
 
@@ -86,6 +99,16 @@ class CoffeeBeanListener : AsyncriptBaseListener() {
 
     override fun enterClassOrInterfaceModifier(ctx: AsyncriptParser.ClassOrInterfaceModifierContext?) {
         code.append("${ctx?.text} ")
+    }
+
+    override fun enterExpectExpression(ctx: AsyncriptParser.ExpectExpressionContext?) {
+        currentFieldNameElement?.end?.let { code.insert(it, "Mono") }
+        currentTypeDeclarationElement?.end?.let { code.insert(it, ">") }
+        currentTypeDeclarationElement?.start?.let { code.insert(it, "Mono<") }
+        currentFieldNameElement?.let {
+            asyncFieldList[it.text] = currentField
+        }
+        code.append(" ${ctx?.expression()?.text} ")
     }
 
     override fun enterAsyncMethodBody(ctx: AsyncriptParser.AsyncMethodBodyContext?) {
@@ -121,3 +144,5 @@ class CoffeeBeanListener : AsyncriptBaseListener() {
     }
 
 }
+
+data class Element(var text: String, var start: Int, var end: Int)
